f93def955a21e2cca06c9e3c2b8fa20654c03241
Chris Boettner <boettnec@gmail.com>
2024-01-31 14:49:11 +0100

    fixed bug in HATS, contiued WASP, added errors for reference file, free stddev for WASP

--- gallifrey/inference/__init__.py
+++ gallifrey/inference/__init__.py
@@ -1,17 +1,5 @@
 from gallifrey.inference.likelihood import log_likelihood_function
-from gallifrey.inference.mcmc import (
-    create_initial_positions,
-    gelman_rubin_diagnostic,
-    inference_algorithm,
-    nuts_warmup,
-    run_mcmc,
-)
 
 __all__ = [
     "log_likelihood_function",
-    "gelman_rubin_diagnostic",
-    "inference_algorithm",
-    "create_initial_positions",
-    "nuts_warmup",
-    "run_mcmc",
 ]
